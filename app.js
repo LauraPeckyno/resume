758403cd778b071fb7f101f308774b153d50bef0
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-07 08:46:56 -0400

    adding a few more elements

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ const addressBlock = document.querySelector('.addressBlock');
 function addressBlocks() {
   let greeting = document.querySelector(".greeting");
   let text =
-    "<h3>Greetings!</h3><p>It’s a pleasure to virtually meet you. While working in the publishing and nonprofit sectors, I saw a real need for technical skills. I’m ready to pivot my career to the tech side, so I can help even more companies and organizations tell their stories with interactive, data-informed tools. I’d love to learn more about your technical needs and the types of clients you serve. How can my insights into publishing and philanthropy add to your toolset?</p>";
+    "<h4>Greetings!</h4><p>It’s a pleasure to virtually meet you. While working in the publishing and nonprofit sectors, I saw a real need for technical skills. I’m ready to pivot my career to the tech side, so I can help even more companies and organizations tell their stories with interactive, data-informed tools. I’d love to learn more about your technical needs and the types of clients you serve. How can my insights into publishing and philanthropy add to your toolset?</p>";
   greeting.innerHTML = text;
 }
 
 addressBlock.addEventListener('mouseover', addressBlocks);
+
+document.body.style.cursor = 'pointer';
